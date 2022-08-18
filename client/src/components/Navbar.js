79841a0b0409d70cf48cbaf512538559fa43d19a
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-18 11:36:25 +0200

    changed text to icons, added logo with link to homepage

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,24 +1,36 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Link} from "react-router-dom";
-
-
+import {CgProfile} from "react-icons/cg";
+import {BsSearch} from "react-icons/bs"
+import {IoHomeSharp} from "react-icons/io5"
+import {FaShoppingCart} from "react-icons/fa"
+import logo from "../images/logo5.PNG"
 //We can use a logo to import to go to the HomePage and for the Profile and Checkout
-const Navbar =() => {
+const Navbar = () => {
     return (
-    <div className='bg-gray-300 flex justify-between items-center border border-solid p-4'>
-        <h1 className='full text-3xl font-bold'>G-Bay</h1>
-        <input className="form-control mx-auto rounded w-60" type="search" placeholder="Search" aria-label="Search"/>
-        <ul className='flex'>
-            <li>
-                <Link className='p-4' to='/'>Home</Link>
-                <Link className='p-4' to='/profile'>Profile</Link>
-                <Link className="p-4" to='/checkout'>Checkout</Link>
+        <div className='bg-gray-300 flex justify-between items-center border border-solid p-4'>
+            <Link to="/">
+                <img className='w-16 h-16'
+                    src={logo}
+                    alt="logo"
+                />
+            </Link>
+                <div className="flex justify-between">
+                    <input className="form-control mx-0 rounded w-60" type="search" placeholder="Search"
+                           aria-label="Search"/>
+                    <BsSearch className="mt-1 ml-2"/>
+                </div>
+                <ul className='flex'>
+                    <li className='flex flex-row'>
+                        <Link className='p-4' to='/'><IoHomeSharp/></Link>
+                        <Link className='p-4' to='/profile'><CgProfile/></Link>
+                        <Link className="p-4" to='/checkout'><FaShoppingCart/></Link>
 
-            </li>
+                    </li>
 
-        </ul>
-    </div>
-    )
+                </ul>
+        </div>
+)
 }
 
 export default Navbar;
