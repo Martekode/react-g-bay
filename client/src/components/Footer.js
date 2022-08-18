5179db25eb82b6722239a0da2cf91b44a959c231
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-18 12:27:32 +0200

    added github links to teammembers in footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
-
+import {FaGithub} from "react-icons/fa";
 class Footer extends Component {
     render() {
         return (
-            <div>
-                <h1>Footer</h1>
+            <footer className='flex justify-evenly items-center h-16'>
                 <p>Copyright @BECODE 2022 </p>
-            </div>
+                <a href='https://github.com/glinchflash' >Glenn <FaGithub className='ml-3'/></a>
+                <a href='https://github.com/YasserB94'>Yasser <FaGithub className='ml-3'/></a>
+                <a href='https://github.com/MichaelMontei'>Michael <FaGithub className='ml-5'/></a>
+                <a href='https://dengian.github.io/Knights-of-The-Pink-Unicorn/'>Ian <FaGithub className='ml-1'/></a>
+                <a href='https://github.com/Martekode'>Brian <FaGithub className='ml-3'/></a>
+            </footer>
         );
     }
 }
