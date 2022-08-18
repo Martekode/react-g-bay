701243553b5b3f09abaf40ed96be5b455fd98b43
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-18 11:46:55 +0200

    Sellpage sketch done

--- client/src/pages/Sellpage.js
+++ client/src/pages/Sellpage.js
@@ -4,14 +4,14 @@ import Footer from "../components/Footer";
 
 const Sellpage =() =>{
     return(
-        <div className='text-center '>
+        <div>
             <Navbar/>
-            <p className='border-2  h-48 w-52 mb-3'>Upload image</p>
-            <div className='grid grid-cols-2'>
-                <p>Product name</p>
-                <p>Product description</p>
-                <p>Product category</p>
-                <p>Product price</p>
+            <p className='border-2  h-48 w-52 mx-auto mt-5 text-center'>Upload image</p>
+            <div className='grid grid-cols-2 justify-between justify-items-center m-5 text-center'>
+                <p className='border-2  h-48 w-52 m-2'>Product name</p>
+                <p className='border-2  h-48 w-52 m-2'>Product description</p>
+                <p className='border-2  h-48 w-52 m-2'>Product category</p>
+                <p className='border-2  h-48 w-52 m-2'>Product price</p>
             </div>
             <Footer/>
         </div>
