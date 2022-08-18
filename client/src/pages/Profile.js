0bd9ab7a8e70b296b78ea95fb6036369fb464dce
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-18 14:03:37 +0200

    added/imported avatar from react

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import {Link} from "react-router-dom";
+import Avatar from 'react-avatar';
 
 function Profile() {
     return (
@@ -11,8 +12,9 @@ function Profile() {
                 <div className='absolute left-1/2 border-2 w-fit h-52 mb-24' >
                     <h1>Product-List with everything you are Selling</h1>
                 </div>
-                    <div className='border-2 rounded-full w-52 h-52 ml-10'>
-                        <div className='static flex flex-col mt-60 ml-10'>
+                    <div className=' w-52 h-52 ml-10'>
+                        <Avatar name='foo bar' className='rounded-full scale-150 ml-7 mt-8'/>
+                        <div className='static flex flex-col mt-40 ml-10'>
                             <div className='p-6'>Name</div>
                             <div className='p-6'>Email</div>
                          <Link to="/sell"><div className='p-6'>Sell products</div></Link>
