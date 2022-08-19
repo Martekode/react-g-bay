2585824e06f6a903a2dfab1be3f8aac892ae7d44
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-19 13:26:38 +0200

    issues with setting amount and calc price fixed

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function Checkout() {
-    let totalPrice = 0
+    const [totalPrice, setTotalPrice] = useState(0)
     const priceArray = []
     const productArray = [
         {name: 'funko-pop', price: 2, amount: 1},
@@ -12,32 +12,42 @@ function Checkout() {
         {name: 't-shirt', price: 11, amount: 1},
     ]
 
-    const handleAmount =(e)=>{
+    const handleAmount = (e) => {
         e.preventDefault()
-        console.log(e.target.value)
-        const result = e.target.value
-        console.log(priceArray);
-        return result
+        console.log(productArray);
+        console.log(e.target.value);
+        productArray[e.target.id].amount = Number(e.target.value)
+        console.log(productArray);
+        handlePrice()
     }
 
+    const handlePrice = () => {
+        productArray.map((product, i) => {
+            priceArray[i] = (product.price * product.amount)
+            console.log(priceArray);
+        })
+        setTotalPrice(priceArray.reduce((a, v) => a+ v, 0))
+        console.log(totalPrice);
+    }
 
     return (
         <div>
             <Navbar/>
             <div className='flex flex-col items-center'>
                 <ul>
-                    {productArray.map((product,i) => {
-                        priceArray.push((product.price*product.amount))
+                    {productArray.map((product, i) => {
+                        priceArray.push(product.amount * product.price)
                         return (
-                                    <li key={i} className='border-2 w-[45rem] h-28 m-1'>name: {product.name} <br/>
-                                        price: {product.price}<br/>
-                                        <input type='number' min='0' defaultValue={product.amount} className='w-12 h-8' onChange={handleAmount}/>
-                                    </li>
+                            <li key={i} className='border-2 w-[45rem] h-28 m-1'>name: {product.name} <br/>
+                                price: {product.price}<br/>
+                                <input type='number' min='0' id={i} defaultValue={product.amount} className='w-12 h-8'
+                                       onChange={handleAmount}/>
+                            </li>
 
                         );
                     })}
                 </ul>
-                <p>Total price : {totalPrice}</p>
+                <p>Total price : {totalPrice? totalPrice : setTotalPrice(priceArray.reduce((a, v) => a+ v, 0))}</p>
             </div>
             <Footer/>
         </div>
