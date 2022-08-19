b53682a0c36dddeac9b231b39e3b1d07e4524138
DenGian <mondelaers.ian@gmail.com>
2022-08-19 15:31:30 +0200

    add product router

--- server/api/routers/productRouter.js
+++ server/api/routers/productRouter.js
@@ -1,7 +1,18 @@
-import
-async doStuff() {
-    const query = `SELECT * FROM product_table`;
-    const result = await this.connection.query(query);
-    console.log(result)
-    return result[0];
-}
+import express from 'express'
+
+const productRouter = express.Router()
+
+import Product from "../models/productModel.js"
+
+productRouter.get('api/products', (_request, response) => {
+    const product = new Product (
+        `SELECT ID FROM product_table`,
+        `SELECT name FROM product_table`,
+        `SELECT price FROM product_table`
+    )
+    response.send('id:' + product.getId())
+    response.send('name:' + product.getName())
+    response.send('price:' + product.getPrice())
+})
+//Make this router accessible to JavaScript Modules
+export default productRouter();
