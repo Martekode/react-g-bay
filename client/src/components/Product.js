b1ea6460dea8a65c1688a81701559a0608d9f4f7
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-19 10:27:30 +0200

    Revert "Merge pull request #1 from Martekode/MM_frontend_features"
    This reverts commit 5d23a2961f75dedd1bd7e1883748a11d09b6010a, reversing
    changes made to a33b12ce93de8bf1578a41bff235fd9f841c5307.

--- client/src/components/Product.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Product (props){
-    return (
-    <h1>{props.productName}{props.productPrice}{props.productAmount}</h1>
-    );
-}
-
-export default Product;
