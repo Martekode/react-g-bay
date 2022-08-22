7161fb2321912f2bfd9c00e9f00414b4345c29bb
Martekode <BrianDesmedt9000@hotmail.com>
2022-08-23 01:01:50 +0200

    added profile comp and loading added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
+import {useAuth0} from '@auth0/auth0-react';
+import {AiOutlineLoading3Quarters} from 'react-icons/Ai'
+import ProfileTest from './components/ProfileTest';
 
 function App() {
+  const {isLoading , error } = useAuth0();
+
 return (
   <main>
     <h1>auth0 Login</h1>
-    <LoginButton/>
-    <LogoutButton/>
+    {error && <p>Authentication Error</p>}
+    {!error && isLoading && <AiOutlineLoading3Quarters/>}
+    {!error && !isLoading && (
+      <>
+        <LoginButton/>
+        <LogoutButton/>
+        <ProfileTest />
+      </>
+    )}
   </main>
 )
 
