85427e7a58a152178d65dc1bd2a4b31a3f5184bb
Yasser Bal <yasserbal@icloud.com>
2022-08-23 13:13:05 +0200

    Improved User endpoint Can now get All Usernames from DB Can now Get user object in more ways Can now create a new User Can now check if a name exists Can now check if an email exists

--- server/api/routers/userRouter.js
+++ server/api/routers/userRouter.js
@@ -6,8 +6,7 @@ const encryptor = require("../helpers/encryption");
 const { request, response } = require("express");
 //BASE PATH - DEV INDICATOR
 router.get("/", (request, response) => {
-  const test = encryptor.encrypt("Hello", "Secret");
-  response.json(test);
+  response.send("Hello From /user");
 });
 /*
   __  ___  ___ 
@@ -47,6 +46,46 @@ router.get("/email/:email", async (request, response) => {
     response.status(handledError.status).json(handledError.message);
   }
 });
+//GET ALL USERNAMES IN DB
+router.get("/name/all", async (request, response) => {
+  try {
+    const result = await user.getAllNames();
+    response.status(200).json(result);
+  } catch (error) {
+    const handledError = errorHandler.handleUserError(error);
+    response.status(handledError.status).json(handledError.message);
+  }
+});
+//SEE IF USERNAME EXISTS - RETURNS TRUE/FALSE DEPENDING IF USERNAME EXISTS
+router.get("/name/check/:name", async (request, response) => {
+  try {
+    const result = await user.getUserByName(request.params.name);
+    if (!result.length) {
+      response.status(200).send(false);
+    }
+    if (result.length) {
+      response.status(200).send(true);
+    }
+  } catch (error) {
+    const handledError = errorHandler.handleUserError(error);
+    response.status(handledError.status).json(handledError.message);
+  }
+});
+//SEE IF EMAIL EXISTS - RETURNS TRUE/FALSE DEPENDING IF EMAIL EXISTS
+router.get("/email/check/:email", async (request, response) => {
+  try {
+    const result = await user.getUserByEmail(request.params.email);
+    if (!result.length) {
+      response.status(200).send(false);
+    }
+    if (result.length) {
+      response.status(200).send(true);
+    }
+  } catch (error) {
+    const handledError = errorHandler.handleUserError(error);
+    response.status(handledError.status).json(handledError.message);
+  }
+});
 /*
  ___  _  __  ___ 
 | o \/ \/ _||_ _|
@@ -54,7 +93,36 @@ router.get("/email/:email", async (request, response) => {
 |_|  \_/|__/ |_|              
 Here we define all Post methods
 */
-router.post("/new", async (request, response) => {});
+router.post("/new", async (request, response) => {
+  try {
+    const { username, email, image_url } = request.body;
+    let imageurl;
+    if (!(username && email)) {
+      throw new Error("undefined");
+    }
+    if (image_url) {
+      imageurl = image_url;
+    }
+    //Check if username exists
+    const nameCheck = await user.getUserByName(username);
+    if (nameCheck.length) {
+      throw new Error("nameAlreadyInDB");
+    }
+    const emailCheck = await user.getUserByEmail(email);
+    if (emailCheck.length) {
+      throw new Error("mailAlreadyInDB");
+    }
+    const result = await user.createNewUser(username, email, imageurl);
+    const responseUser = await user.getUserByID(result.insertId.toString());
+    response.status(200).json({
+      "DbId:": result.insertId.toString(),
+      "UserObject:": responseUser[0],
+    });
+  } catch (error) {
+    const handledError = errorHandler.handleUserError(error);
+    response.status(handledError.status).json(handledError.message);
+  }
+});
 
 /*
  _(`-')    (`-')  _         (`-')  _(`-')      (`-')  _ 
