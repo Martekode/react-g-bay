53fee9cd7fb06a6d98a85bab020cee72115b702b
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-25 12:18:36 +0200

    change profile page according to user being logged in or not

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,27 +1,18 @@
-import {Link} from "react-router-dom";
-import Avatar from 'react-avatar';
+import UnAuthenticatedProfile from "../components/UnAuthenticatedProfile";
+import AuthenticatedProfile from "../components/AuthenticatedProfile";
+
 
 function Profile() {
+
     return (
         <div>
-            <div className='w-100% h-screen'>
-                <p className='text-2xl ml-24 mt-24'>Profile Pic</p>
-                <div className='absolute left-1/2 border-2 w-fit h-52 mb-24' >
-                    <h1>Product-List with everything you are Selling</h1>
-                </div>
-                    <div className=' w-52 h-52 ml-10'>
-                        <Avatar name="Tim Broos" className='rounded-full scale-150 ml-7 mt-8'/>
-                        <div className='static flex flex-col mt-32 ml-10'>
-                            <div className='p-6'>Tim Broos</div>
-                            <div className='p-6'>tim.broos@becode.org</div>
-                         <Link to="/sell"><div className='p-6 hover:text-hovers' >Sell products</div></Link>
-                        </div>
-                    </div>
-                <div className='absolute left-1/2 border-2 w-fit h-52 mt-12' >
-                    <h1>Wishlist for items that user wants for Selling</h1>
-                </div>
-            </div>
+            <h1>Login page</h1>
+            <>
+                <UnAuthenticatedProfile/>
+                <AuthenticatedProfile/>
+            </>
         </div>
     )
 }
+
 export default Profile
