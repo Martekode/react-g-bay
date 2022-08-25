f9785400ee5f63858e3a161bd516d1739b38e157
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-25 10:13:00 +0200

    removed amount from products and prize calculation

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -5,29 +5,12 @@ function Checkout() {
     const [totalPrice, setTotalPrice] = useState(0)
     const priceArray = []
     const productArray = [
-        {name: 'funko-pop', price: 2, amount: 1},
-        {name: 'action figure', price: 4, amount: 1},
-        {name: 'tcg-Cards', price: 15, amount: 1},
-        {name: 't-shirt', price: 11, amount: 1},
+        {name: 'funko-pop', price: 2},
+        {name: 'action figure', price: 4},
+        {name: 'tcg-Cards', price: 15},
+        {name: 't-shirt', price: 11},
     ]
 
-    const handleAmount = (e) => {
-        e.preventDefault()
-        console.log(productArray);
-        console.log(e.target.value);
-        productArray[e.target.id].amount = Number(e.target.value)
-        console.log(productArray);
-        handlePrice()
-    }
-
-    const handlePrice = () => {
-        productArray.map((product, i) => {
-            priceArray[i] = (product.price * product.amount)
-            console.log(priceArray);
-        })
-        setTotalPrice(priceArray.reduce((a, v) => a + v, 0))
-        console.log(totalPrice);
-    }
 
     return (
         <div>
@@ -35,12 +18,10 @@ function Checkout() {
             <div className='flex flex-col items-center mt-12'>
                 <ul>
                     {productArray.map((product, i) => {
-                        priceArray.push(product.amount * product.price)
+                        priceArray.push(product.price)
                         return (
                             <li key={i} className='border-2 w-[45rem] h-28 m-1'>name: {product.name} <br/>
                                 price: {product.price}<br/>
-                                <input type='number' min='0' id={i} defaultValue={product.amount} className='w-12 h-8 bg-background'
-                                       onChange={handleAmount}/>
                             </li>
 
                         );
