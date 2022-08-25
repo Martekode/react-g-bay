4d2c3dbee0cae967b2dc2f6c3b62c5ca47821e76
DenGian <mondelaers.ian@gmail.com>
2022-08-25 14:17:21 +0200

    add endpoints Getters for products

--- server/api/models/Product.js
+++ server/api/models/Product.js
@@ -30,6 +30,15 @@ class Product {
     const query = "SELECT * FROM product_table WHERE category = ?";
     return this.pool.query(query, [category]);
   }
+  async getAllProductsByOwnerId(userid) {
+    const query = "SELECT name FROM product_table WHERE owner_id = ?";
+    return this.pool.query(query, userid);
+  }
+  async getAllProductsByEmail(email) {
+    const query = "SELECT product_table.id, owner_id, product_table.name, price, description, product_table.image_url, category FROM product_table INNER JOIN user_table ON user_table.id = product_table.owner_id WHERE user_table.email = ?";
+    return this.pool.query(query, [email])
+  }
+
   /*
  ___  _  __  ___
 | o \/ \/ _||_ _|
@@ -62,6 +71,34 @@ Here we define all Post methods
   async deleteProductById(id) {
     const query = "DELETE FROM product_table WHERE id = ?";
     return this.pool.query(query, [id]);
+  }
+  /*
+_     ____  ____  ____  _____  _____
+/ \ /\/  __\/  _ \/  _ \/__ __\/  __/
+| | |||  \/|| | \|| / \|  / \  |  \
+| \_/||  __/| |_/|| |-||  | |  |  /_
+\____/\_/   \____/\_/ \|  \_/  \____\
+
+*/
+  async updateProductName(id, newName) {
+    const query = "UPDATE product_table SET name = ? WHERE id = ?";
+    return this.pool.query(query, [newName, id]);
+  }
+  async updateProductPrice(id, newPrice) {
+    const query = "UPDATE product_table SET price = ? WHERE id = ?";
+    return this.pool.query(query, [newPrice, id]);
+  }
+  async updateProductDescription(id, newDescription) {
+    const query = "UPDATE product_table SET description = ? WHERE id = ?";
+    return this.pool.query(query, [newDescription, id]);
+  }
+  async updateProductImageUrl(id, newImageUrl) {
+    const query = "UPDATE product_table SET image_url = ? WHERE id = ?";
+    return this.pool.query(query, [newImageUrl, id]);
+  }
+  async updateProductCategory(id, newCategory) {
+    const query = "UPDATE product_table SET category = ? WHERE id = ?";
+    return this.pool.query(query, [newCategory, id]);
   }
 }
 const product = new Product();
