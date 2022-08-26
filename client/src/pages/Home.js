7ee22e1366a64a129f106f86c9fa7bf2b84eb3e3
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-26 10:52:44 +0200

    finished email check to see if user already exists in DB,and push new user to database accordingly

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,17 @@
-import React,{useEffect, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {useAuth0} from '@auth0/auth0-react';
 
 const handleClick = event => {
-    window.open("http://localhost:3050/category","_self");
+    window.open("http://localhost:3050/category", "_self");
 };
 
 
-const Home =() => {
+const Home = () => {
     const {isAuthenticated, user} = useAuth0();
     const url = 'http://localhost:3050/api/user/new'
-    const [data, setData] = useState({
-        username: "",
-        email: "",
-        image_url: "",
-    })
+    const checkForMailUrl = 'http://localhost:3050/api/user/check/email'
+    const [emailBoolean, setEmailBoolean] = useState(true);
 
     const refactorUser = (oldUser) => {
         let newUser = {
@@ -23,18 +20,32 @@ const Home =() => {
             image_url: oldUser.picture,
         };
         console.log(newUser);
-        setData(newUser)
-        axios.post(url, newUser).then(res => {
-            console.log(res.data)
+        axios.post(checkForMailUrl, newUser).then(res => {
+            console.log(res.data);
+            setEmailBoolean(res.data)
         })
+
     }
+
+    useEffect(() => {
+        if (!emailBoolean && user) {
+            let newUser = {
+                username: user.nickname,
+                email: user.email,
+                image_url: user.picture,
+            };
+            axios.post(url, newUser).then(res => {
+                console.log(res.data)
+            })
+        }
+    }, [emailBoolean])
 
     useEffect(() => {
         if (isAuthenticated) {
             refactorUser(user)
         }
-        console.log(data);
     }, [user])
+
 
     return (
 
@@ -44,44 +55,44 @@ const Home =() => {
                 <div className='grid grid-cols-4 justify-items-center items-center mt-20'>
 
 
-                    <p> <img onClick={handleClick}
-                        src="https://images.unsplash.com/photo-1613771404721-1f92d799e49f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
-                        className="p-4 h-80 max-w-md mt-12 pr-4" alt="test"></img>
+                    <p><img onClick={handleClick}
+                            src="https://images.unsplash.com/photo-1613771404721-1f92d799e49f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80"
+                            className="p-4 h-80 max-w-md mt-12 pr-4" alt="test"></img>
                     </p>
 
-                    <p>  <img onClick={handleClick}
-                              src="https://images.unsplash.com/photo-1593814681464-eef5af2b0628?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                              className="h-auto max-w-md mt-12 pr-4" alt="test"></img>
+                    <p><img onClick={handleClick}
+                            src="https://images.unsplash.com/photo-1593814681464-eef5af2b0628?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                            className="h-auto max-w-md mt-12 pr-4" alt="test"></img>
                     </p>
 
-                    <p>  <img onClick={handleClick}
-                              src="https://images.unsplash.com/photo-1610888301741-26d484c42647?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80"
-                              className="h-72 max-w-md h-62 w-62 mt-12 pr-4" alt="test"></img>
+                    <p><img onClick={handleClick}
+                            src="https://images.unsplash.com/photo-1610888301741-26d484c42647?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1365&q=80"
+                            className="h-72 max-w-md h-62 w-62 mt-12 pr-4" alt="test"></img>
                     </p>
 
-                    <p>  <img onClick={handleClick}
-                              src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                              className="h-auto max-w-md mt-12 pr-4" alt="test"></img>
+                    <p><img onClick={handleClick}
+                            src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                            className="h-auto max-w-md mt-12 pr-4" alt="test"></img>
                     </p>
 
-                    <p>  <img onClick={handleClick}
-                              src="https://images.unsplash.com/photo-1587202372634-32705e3bf49c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
-                              className="h-auto max-w-md mt-12 pr-4 mb-32" alt="test"></img>
+                    <p><img onClick={handleClick}
+                            src="https://images.unsplash.com/photo-1587202372634-32705e3bf49c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                            className="h-auto max-w-md mt-12 pr-4 mb-32" alt="test"></img>
                     </p>
 
-                    <p>  <img onClick={handleClick}
-                              src="https://images.unsplash.com/photo-1613376023733-0a73315d9b06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                              className="h-auto max-w-md mt-12 pr-4 mb-32" alt="test"></img>
+                    <p><img onClick={handleClick}
+                            src="https://images.unsplash.com/photo-1613376023733-0a73315d9b06?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                            className="h-auto max-w-md mt-12 pr-4 mb-32" alt="test"></img>
                     </p>
 
-                    <p>  <img onClick={handleClick}
-                              src="https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                              className="h-auto max-w-md mt-12 pr-4 mb-32" alt="test"></img>
+                    <p><img onClick={handleClick}
+                            src="https://images.unsplash.com/photo-1612036782180-6f0b6cd846fe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                            className="h-auto max-w-md mt-12 pr-4 mb-32" alt="test"></img>
                     </p>
 
-                    <p>  <img onClick={handleClick}
-                              src="https://images.unsplash.com/photo-1606503153255-59d8b8b82176?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
-                              className="h-auto max-w-md mt-12 pr-4 mb-32" alt="test"></img>
+                    <p><img onClick={handleClick}
+                            src="https://images.unsplash.com/photo-1606503153255-59d8b8b82176?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+                            className="h-auto max-w-md mt-12 pr-4 mb-32" alt="test"></img>
                     </p>
 
                     {/*<p>  <img onClick={handleClick}*/}
