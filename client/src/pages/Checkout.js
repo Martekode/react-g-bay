4b17356738e34a1c0bf31f3ee702848ef7888203
glinchflash <102614947+glinchflash@users.noreply.github.com>
2022-08-26 16:52:30 +0200

    retrieve information from Card page and display the items with price + get information of these items from database

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -1,15 +1,31 @@
 import React, {useState} from "react";
-
+import {useRecoilValue} from "recoil";
+import {checkoutArrayState} from "../App";
+import axios from "axios";
 
 function Checkout() {
     const [totalPrice, setTotalPrice] = useState(0)
-    const priceArray = []
-    const productArray = [
-        {name: 'funko-pop', price: 2},
-        {name: 'action figure', price: 4},
-        {name: 'tcg-Cards', price: 15},
-        {name: 't-shirt', price: 11},
-    ]
+    const priceArray = [];
+    const CheckoutArrayValue = useRecoilValue(checkoutArrayState);
+
+
+    CheckoutArrayValue.forEach((value) => {
+        console.table(value);
+        console.time(value);
+    })
+
+    const buySingleItem = (e) => {
+        const product = JSON.parse(e.target.value);
+        console.log('Chosen product: ');
+        console.table(product)
+        const productId = product.id;
+        const ownerID = product.owner_id;
+        console.log('product Information: ');
+        console.log(productId);
+        console.log(ownerID);
+    }
+
+
 
 
     return (
@@ -17,17 +33,21 @@ function Checkout() {
             <p className='flex justify-center items-center text-6xl mt-24 font-bangers'>Checkout</p>
             <div className='flex flex-col items-center mt-12 font-bangers text-xl'>
                 <ul>
-                    {productArray.map((product, i) => {
-                        priceArray.push(product.price)
+                    {CheckoutArrayValue.map((product, i) => {
+                        priceArray.push(parseInt(product.price))
                         return (
                             <li key={i} className='border-2 w-[45rem] h-28 m-1'>name: {product.name} <br/>
                                 price: {product.price}<br/>
+                                <button value={JSON.stringify(product)} onClick={buySingleItem} className="border-2 border-solid mt-6 hover:bg-hovers p-2 w-20 h-10 flex justify-center items-center rounded-full">Buy this item right
+                                    now!
+                                </button>
                             </li>
 
                         );
                     })}
                 </ul>
-                <p className='mt-12 mb-12 text-3xl font-bangers'>Total price : {totalPrice ? totalPrice : setTotalPrice(priceArray.reduce((a, v) => a + v, 0)) }</p>
+                <p className='mt-12 mb-12 text-3xl font-bangers'>Total price
+                    : {totalPrice ? totalPrice : setTotalPrice(priceArray.reduce((a, v) => a + v, 0))}</p>
             </div>
         </div>
     )
